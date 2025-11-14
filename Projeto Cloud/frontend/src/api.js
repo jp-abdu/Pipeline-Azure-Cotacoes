fdb483b5d0408abe76b57f376e7f037bc7982089
jp-abdu <joaopedroabdu@gmail.com>
2025-11-14 13:08:40 -0300

    at

--- Projeto Cloud/frontend/src/api.js
+++ Projeto Cloud/frontend/src/api.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-export const api = axios.create({ baseURL: 'https://projeto-cloud-b3-fkhpfyhufbd4hbbk.brazilsouth-01.azurewebsites.net/api' });
+export const api = axios.create({ baseURL: 'https://webapp-backend-b3-pipeline.azurewebsites.net/api' });
 export const fetchAssets = async (filters) => {
   const res = await api.get('/assets', { params: filters });
   return res.data;
