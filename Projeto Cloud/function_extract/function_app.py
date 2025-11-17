63bb2830a390ccd8c2fa5d0b7461271d0b4fea03
Fellipe Valladares <fellipevalladares@gmail.com>
2025-11-17 03:27:58 -0300

    Extract rodando as 8 e as 20 e 30minutos inves de a cada 30 segundos

--- Projeto Cloud/function_extract/function_app.py
+++ Projeto Cloud/function_extract/function_app.py
@@ -3,8 +3,8 @@ import azure.functions as func
 import extract as extract
 
 app = func.FunctionApp()
-@app.timer_trigger(schedule="*/30 * * * * *", arg_name="myTimer", run_on_startup=False,
-              use_monitor=False) 
+@app.timer_trigger(schedule="0 30 8,20 * * *", arg_name="myTimer", run_on_startup=False,
+              use_monitor=False)
 def extract_arquivos_trigger(myTimer: func.TimerRequest) -> None:
     logging.info('Executando extração de arquivos B3...')
     # Chama a função de extração
